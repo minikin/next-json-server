6dcbf190479bcef15984d9ad5a442a4cfb13f16c
Sasha Prokhorenko <djminikin@gmail.com>
2020-01-06 14:33:09 +0100

    Update db, server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
 const http = require("http");
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
 
 const port = process.env.PORT || 4000;
 
-server.use(middlewares)
+server.db = router.db
+server.use(auth)
 server.use(router)
+
 server.listen(port, () => {
   console.log('JSON Server is running')
 })
@@ -15,3 +17,7 @@ server.listen(port, () => {
 setInterval(function () {
   http.get('https://next-json-server.herokuapp.com');
 }, 300000);
+
+// setInterval(function () {
+//   http.get('http://localhost');
+// }, 300000);
